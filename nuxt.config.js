bd7af1fa17d214415a815b57cbe5b2d3a3b9fbce
Mücahit Nezir <mucahit.nezir@mosstar.com.tr>
2023-04-13 13:34:45 +0300

    disable ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 export default {
+  // The ssr property: https://nuxtjs.org/docs/configuration-glossary/configuration-ssr
+  ssr: false,
+
+  // The target property: https://nuxtjs.org/docs/configuration-glossary/configuration-target
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Auth Flow',
